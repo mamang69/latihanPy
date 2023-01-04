e8c4a0ab10a7d51d5e1052937be37ec759be9134
mamang69 <203040031@mail.unpas.ac.id>
2023-01-04 15:04:48 +0700

    mempelajari operasi aritmatika

---
+++ 08-Aritmatika/main.py
@@ -0,0 +1,42 @@
+# operasi aritmatika
+
+a = 10
+b = 3
+
+# operasi tambah
+
+# operasi pertambahan
+hasil = a+b
+print(a, "+", b, " = ", hasil)
+
+# operasi pengurangan
+hasil = a-b
+print(a, "-", b, " = ", hasil)
+
+# operasi perkalian
+hasil = a*b
+print(a, "x", b, " = ", hasil)
+
+# operasi pembagian
+hasil = a/b
+print(a, ":", b, " = ", hasil)
+
+# operasi eksponen(perpangkatan) **
+hasil = a**b
+print(a, "^", b, " = ", hasil)
+
+# operasi mmodulus %
+hasil = a % b
+print(a, "mod", b, " = ", hasil)
+
+# operasi floor division(pembulatan hasil bagi(kebawah)) //)
+hasil = a // b
+print(a, "//", b, " = ", hasil)
+
+# prioritas operasi di PY
+'''
+    1. ()
+    2. eksponen
+    3. kali,bagi,mod,floor
+    4. penjumlahan dan pengurangan
+'''
