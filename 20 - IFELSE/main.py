ddfe39d5e91c2c51ee08539b3e891383c6faf2a2
mamang69 <elditamvan15@gmail.com>
2023-02-14 13:00:45 +0700

    menyelesaikan if else

---
+++ 20 - IFELSE/main.py
@@ -0,0 +1,20 @@
+# IF ELSE
+
+nama = input("nama kamu siapa ? ")
+
+# 1. If inline
+if nama == "Eldi":
+    print("halo ")
+print("Terima kasih", nama)
+
+# 2. if indentation
+if nama == "Eldi":
+    print("kamu ganteng abiez")
+    print("Kamu juga keren")
+print("P")
+
+# 3. Else
+if nama == "Eldi":
+    print("Halo bang ganteng")
+else:
+    print("LO SIAPA COOK ??")
---
+++ 21 - ELIF/main.py
@@ -0,0 +1,11 @@
+# ELIF
+
+nama = input("Nama Kamu Siapa ?")
+
+if nama == "Eldi":
+    print("Halo bang ganteng")
+elif nama == "Jona":
+    print("Shaaap")
+else:
+    print("LO SIAPAA ??")
+print("End Program")
